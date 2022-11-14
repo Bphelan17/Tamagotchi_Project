8cff2ea9c3fbccc1279cdb224d7cff8a6a1f41e3
Bphelan17 <bphelan17@yahoo.com>
2022-11-14 16:37:37 -0700

    changes to age function, add hunger function

--- main.js
+++ main.js
@@ -1,23 +1,54 @@
-// create object for pet
-const tamagotchi = {
+// // create object for pet
+// const tamagotchi = {
+//     age: 0,
+//     start() {
+        
+//     }
+// }
+
+// // const increaseAge = document.querySelector('h3')
+
+
+
+
+// const timeAge = () => {
+//     tamagotchi.age += 1;
+//     console.log(tamagotchi);
+// }
+
+
+// const ageInterval = setInterval(timeAge, 1000);  
+
+const tamagotchiAge = {
     age: 0,
-    // hungerLevel: 1,
-    // sleepyLevel: 1,
-    // boredomLevel: 1,
+    start() {
+        const timeAge = setInterval( () => {
+            this.age++
+            console.log(this.age)
+            if(this.age === 100) {
+                console.log('Your Pet Died!')
+                clearInterval(timeAge)
+            }
+        }, 50000);
+    }
 }
 
-// const increaseAge = document.querySelector('h3')
-
-
-
-
-const timeAge = () => {
-    tamagotchi.age += 1;
-    console.log(tamagotchi);
+const tamagotchiHunger = {
+    hunger: 0,
+    start() {
+        const hungerLevel = setInterval( () => {
+            this.hunger++
+            console.log(this.hunger)
+            if(this.hunger === 10) {
+                console.log('Your Pet Died!')
+                clearInterval(hungerLevel)
+            }
+        }, 10000);
+    }
 }
 
-
-const ageInterval = setInterval(timeAge, 1000);  
+console.log(tamagotchiAge.start())
+console.log(tamagotchiHunger.start())
 
    
 
