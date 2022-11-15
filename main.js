0ef066df2a9cbb7215d92919d787640bc1c281ea
Bphelan17 <bphelan17@yahoo.com>
2022-11-14 20:09:29 -0700

    chnages hunger object to a function

--- main.js
+++ main.js
@@ -21,31 +21,48 @@ const tamagotchiAge = {
                 console.log('Your Pet Died!')
                 clearInterval(timeAge)
             }
-        }, 60000);
+        }, 500);
     }
 }
 
+const deathMessage = document.querySelector("#pet-dead")
+
 const showHunger = document.querySelector("#Hunger")
 
-const tamagotchiHunger = {
-    hunger: 0,
-    start() {
+
+const tamagotchiHunger = () => {
+    hunger = 0
+    function start() {
         const hungerLevel = setInterval( () => {
             showHunger.textContent = this.hunger++
             console.log(this.hunger)
             if(this.hunger === 11) {
-                console.log('Your Pet Died!')
                 clearInterval(hungerLevel)
+                deathMessage.textContent = 'Your Pet Died!!!'
             }
-        }, 10000);
+        }, 100);
     }
+    function fixHunger () {
+        showHunger.textContent = this.hunger--
+    }
+    start()
+    fixHunger()
+    const btnFeed = document.querySelector("#btn-feeder")
+    btnFeed.addEventListener('click', fixHunger)
 }
+tamagotchiHunger()
 
-const btnFeed = document.querySelector("#btn-feeder")
-btnFeed.addEventListener('click', handleStartClick)
+// function fixHunger () {
+    
+//     showHunger.textContent = this.hunger--
+// }
+
+
+
+
 
 function handleStartClick() {
-    console.log('you have fed your pet')
+    // console.log('you have fed your pet')
 }
 
 
@@ -71,7 +88,7 @@ const btnSleep = document.querySelector("#btn-sleeper")
 btnSleep.addEventListener('click', handleStartClick)
 
 function handleStartClick() {
-    console.log('Your pet is sleeping, shhhh')
+    // console.log('Your pet is sleeping, shhhh')
 }
 
 
@@ -95,15 +112,15 @@ const btnPlayTime = document.querySelector("#btn-play")
 btnPlayTime.addEventListener('click', handleStartClick)
 
 function handleStartClick() {
-    console.log('Your pet is wagging their tail and fetching the ball')
+    // console.log('Your pet is wagging their tail and fetching the ball')
 }
 
 
 
-console.log(tamagotchiAge.start())
-console.log(tamagotchiHunger.start())
-console.log(tamagotchiSleep.start())
-console.log(tamagotchiBoredom.start())
+// console.log(tamagotchiAge.start())
+// console.log(tamagotchiHunger.start())
+// console.log(tamagotchiSleep.start())
+// console.log(tamagotchiBoredom.start())
 
 
    
